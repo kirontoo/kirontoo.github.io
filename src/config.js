4e2176be4affeda2439e8ad4e36a9cccfd0b5ba7
Amy Dang <kirontoo@gmail.com>
2022-09-29 15:18:07 -0700

    update linkedin link

--- src/config.js
+++ src/config.js
@@ -16,7 +16,7 @@ module.exports = {
     // },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/amy-nguyen-dang',
+      url: 'https://www.linkedin.com/in/amyndang',
     },
     // {
     //   name: 'Codepen',
